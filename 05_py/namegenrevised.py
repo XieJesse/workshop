bd79f46732fd73433acd62661a10a822893a6c3c
JesseStuy20 <jxie20@stuy.edu>
2021-09-27 21:32:57 -0400

    Updated amalgamation code

--- 05_py/namegenrevised.py
+++ 05_py/namegenrevised.py
@@ -1,28 +1,37 @@
-#Jesse Xie
+#Jesse Xie, Qina Liu, Patrick Ging
 #SoftDev
-#Python Revision
-#2021-09-26
+#K05: Better Living Through Amalgamation
+#2021-09-27
+
+# SUMMARY OF TRIO DISCUSSION
+# It's not always necessary to utilize a function to accomplish a task, and simplicity is key
+# It's easier to stick to the basics without using outside programs
+# Work smarter, not harder
+# DISCOVERIES
+# Data can be imported from different places so that the code does not be need to be updated
+# QUESTIONS
+# Is it smarter or just more work to read a file in the context of this assignment?
+# COMMENTS
+# I thought it was interesting how people retrieved the names from github in different ways
 
 import random
 
-#function to get a random index from the list
-def findRandom(arr):
-    length = len(arr)
-    index = random.randint(0,length-1) #finds random index from the array, inclusive
-    print(arr[index])
+pd1 = [
+    "Alejandro Alonso", "Aryaman Goenka", "Angela Zhang", "Christopher Liu", "Deven Maheshwari",
+    "Emma Buller", "Ella Krechmer", "Gavin McGinley", "Haotian Gan", "Ivan Lam", "Ishraq Mahid",
+    "Ivan Mijacika", "Julia Nelson", "Lucas Lee", "Lucas Tom Wong", "Michelle Lo", "Oscar Wang",
+    "Owen Yaggy", "Renggeng Zheng", "Shriya Anand", "Shyne Choi", "Sadid Ethun", "Tomas Acuna", "Theo Fahey",
+    "Tina Nguyen", "Tami Takada", "William Chen", "Yusuf Elsharawy", "Zhaoyu Lin"
+    ]
 
-pd1 = ["Alejandro Alonso", "Aryaman Goenka", "Angela Zhang", "Christopher Liu", "Deven Maheshwari",
-       "Emma Buller", "Ella Krechmer", "Gavin McGinley", "Haotian Gan", "Ivan Lam", "Ishraq Mahid",
-       "Ivan Mijacika", "Julia Nelson", "Lucas Lee", "Lucas Tom Wong", "Michelle Lo", "Oscar Wang",
-       "Owen Yaggy", "Renggeng Zheng", "Shriya Anand", "Shyne Choi","Sadid Ethun", "Tomas Acuna","Theo Fahey",
-       "Tina Nguyen", "Tami Takada", "William Chen", "Yusuf Elsharawy", "Zhaoyu Lin"]
+print(pd1[random.randint(0, len(pd1)-1)]) #pd1 name generation
 
-findRandom(pd1) #pd1 name generation
+pd2 = [
+    "Alif Abdullah", "Andrew Juang", "Andy Lin", "Austin Ngan", "Annabel Zhang", "Daniel Sooknanan",
+    "Eric Guo", "Eliza Knapp", "Hebe Huang", "Han Zhang", "Yoonah Chang", "Josephine Lee", "Jonathan Wu",
+    "Jesse Xie", "Justin Zou", "Kevin Cao", "Liesel Wong", "Michael Borczuk", "Mark Zhu", "Noakai Aronesty",
+    "Patrick Ging", "Qina Liu", "Rachel Xiao", "Raymond Yeung", "Sophie Liu", "Shadman Rakib", "Thomas Yu",
+    "Wenhao Dong", "Yaying Liang Li", "Yuqing Wu"
+    ]
 
-pd2 = ["Alif Abdullah", "Andrew Juang", "Andy Lin", "Austin Ngan", "Annabel Zhang", "Daniel Sooknanan",
-       "Eric Guo", "Eliza Knapp", "Hebe Huang", "Han Zhang", "Yoonah Chang", "Josephine Lee", "Jonathan Wu",
-       "Jesse Xie", "Justin Zou", "Kevin Cao", "Liesel Wong", "Michael Borczuk", "Mark Zhu", "Noakai Aronesty",
-       "Patrick Ging","Qina Liu", "Rachel Xiao", "Raymond Yeung", "Sophie Liu", "Shadman Rakib","Thomas Yu",
-       "Wenhao Dong", "Yaying Liang Li", "Yuqing Wu"]
-
-findRandom(pd2) #pd2 name generation
+print(pd2[random.randint(0, len(pd2)-1)]) #pd2 name generation
