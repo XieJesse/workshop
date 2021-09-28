e480bcd4f43a2fd9c87d9038ad2c53ee748ae81c
JesseStuy20 <jxie20@stuy.edu>
2021-09-28 17:14:16 -0400

    renamed folder and added heading

--- 06_py-csv/weightedrandom.py
+++ 06_py-csv/weightedrandom.py
@@ -1,3 +1,8 @@
+#Team Polar - Jesse Xie, Rachel Xiao, Yuqing Wu
+#SoftDev
+#K06: StI/O: Divine your Destiny!
+#2021-09-28
+
 import csv
 
 occupations = {}
@@ -5,6 +10,7 @@ occupations = {}
 with open('occupations.csv') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
     for row in reader:
-        occupations[row[0]] = row[1]
+        occupations[row[0]] = float(row[1])
+        
         
 print(occupations) 
