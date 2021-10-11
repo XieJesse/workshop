afe4ea9b8c5b35e62e198d7f36c704320d9de5a5
JesseStuy20 <jxie20@stuy.edu>
2021-10-11 16:09:51 -0400

    updated files to include links and percentages in data

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/occupyflaskst")
 def main():
-    return render_template("tablified.html", occ=occupations.random_occupation(),joblist = occupations.jobs)
+    return render_template("tablified.html", occ=occupations.random_occupation(), data=occupations.data)
 
 if __name__ == "__main__":
     app.debug = True
--- 13_tempwork/occupations.py
+++ 13_tempwork/occupations.py
@@ -1,16 +1,23 @@
 import csv
 import random
 
+# format: {
+#   [job]: ([percentage], [link])
+# }
+# The key is the job class, the value is a tuple of the percentage and link
+
 data = {}
 
 with open("data/occupations.csv") as f:
     reader = csv.reader(f, delimiter=",")
     for row in reader:
         if row[0] != "Job Class" and row[0] != "Total":
-            data[row[0]] = float(row[1])
+            data[row[0]] = (float(row[1]), row[2])
 
 jobs = list(data.keys())
-weights = list(data.values())
+weights = []
+for value in data.values():
+    weights.append(value[0])
 
 def random_occupation():
     return random.choices(jobs, weights=weights)[0]
