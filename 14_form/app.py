592fadfd49dd2b691941371ab917e498a198962a
JesseStuy20 <jxie20@stuy.edu>
2021-10-17 19:08:35 -0400

    added changes after peer edit

--- 14_form/app.py
+++ 14_form/app.py
@@ -1,6 +1,8 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2021 
+# Team threeCoffeePeanuts: Jesse Xie, Yaying Liang Li, Ryan Wang
+#SoftDev
+#K14 -- Form and Function
+#2021-10-14
+
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -26,37 +28,50 @@ PROTIP: Insert your own in-line comments wherever they will help your future sel
 def disp_loginpage():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
-    print(app)
+    print(app) #<Flask 'app'>
     print("***DIAG: request obj ***")
     print(request)
     print("***DIAG: request.args ***")
     print(request.args)
+    #empty dict cuz user hasn't submitted anything yet
+
+    #request.args['username'] doesn't exist yet, cuz user didn't submit username yet
     #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
+    '''
+        form prompts user to enter username in webpage;
+        <name="username"> saves the user's inputted name under the key "username" in the ImmutableMultiDict
+        <form action="/auth"> after user submits, takes the user to <host>/auth?username=...
+    '''
     return render_template( 'login.html' )
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
 def authenticate():
-    print("\n\n\n")
+    print("\n\n\n") #new lines in terminal
     print("***DIAG: this Flask obj ***")
     print(app)
-    print("***DIAG: request obj ***")
-    print(request)
+    print("***DIAG: request obj ***") #gives you actual http link of page you're on
+    print(request) #<Request 'http://127.0.0.1:5000/auth?username=ajdskdksa&sub1=Submit' [GET]>
     print("***DIAG: request.args ***")
-    print(request.args)
+    print(request.args) #ImmutableMultiDict([('username', 'ajdskdksa'), ('sub1', 'Submit')])
     #print("***DIAG: request.args['username']  ***")
     #print(request.args['username'])
     print("***DIAG: request.headers ***")
-    print(request.headers)
-    
-    username = request.args['username']
+    print(request.headers) #Host: 127.0.0.1:5000
+
+    username = request.args['username'] #access value of key 'username' in Dict
+    '''
+        use response.html as template
+        method (called in response.html) is actually the request.method (which is get)
+        username (called in response.html) is actually username (from this python file)
+    '''
     return render_template('response.html', method=request.method, username=username)  #response to a form submission
 
-    
+
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
-    app.debug = True 
+    app.debug = True
     app.run()
