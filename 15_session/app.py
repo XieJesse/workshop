f00955aa21c401f7ab97b7064b596281bcb319c1
JesseStuy20 <jxie20@stuy.edu>
2021-10-19 21:40:56 -0400

    Updated code from group work, added random keys, and updated notes text file

--- 15_session/app.py
+++ 15_session/app.py
@@ -3,41 +3,69 @@
 #K15 -- Sessions Greetings
 #2021-10-18
 
-
+from os import urandom               #random key generation module
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
 from flask import request           #facilitate form submission
-from flask import sessions
+from flask import session           #module for session capability
 
 #the conventional way:
 #from flask import Flask, render_template, request
 
 app = Flask(__name__)    #create Flask object
 
+#making secret key
+app.secret_key = urandom(32)
 
-@app.route("/") #, methods=['GET', 'POST'])
+#hard coding single username and password
+real_user = "coffee"
+real_passwd = "peanut"
+
+
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
-
-    return render_template( 'login.html' )
+    print(app.secret_key)
+    if not session.get(real_user):
+        # if session doesn't have the correct login info, i.e. you are not signed in
+        return render_template('login.html')
+    else:
+        # if you are signed in, go to signed-in page
+        return render_template("response.html", username=real_user)
 
 
 @app.route("/auth") # , methods=['GET', 'POST'])
 def authenticate():
-    #hard coding single username and password
-    logins = {
-        "coffee":"peanut"
-    }
-    username = request.args['username']
-    password = request.args['password']
+    # using conditional in order to make GET/POST fail-safe
+    if request.method == "GET":
+        username = request.args['username']
+        password = request.args['password']
+    elif request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
 
-    if (username in logins and logins[username] == password):
-        return render_template('response.html', method=request.method, username=username, password = password)
-    elif (username in logins and logins[username] != password):
+    print(session)
+    # if username and password are a correct pair, we send them to response.html (logs you in)
+    if (username == real_user and password == real_passwd):
+        session[username] = password
+        return render_template('response.html', method=request.method, username=username)
+
+    # login failed because of wrong pass, tells you error at the bottom (at login.html)
+    elif (username == real_user and password != real_passwd):
         error = "user exists but wrong password"
         return render_template( 'login.html', error=error)
+
+    # login fails because username isn't correct / doesn't exist
     else:
         error = "Error: that username does not exist"
         return render_template( 'login.html', error=error)
+
+
+
+@app.route("/logout") # , methods=['GET', 'POST'])
+def logout():
+    if real_user in session:
+        session.pop(real_user)
+    return render_template('login.html')
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
