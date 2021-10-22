d2d903cf36292156550389178c6adc6829195847
JesseStuy20 <jxie20@stuy.edu>
2021-10-22 09:19:55 -0400

    added function to populate table

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -13,6 +13,14 @@ db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
 #==========================================================
+def populate(filename, tablename):
+    with open(filename) as csv_file: # read students csv file
+        reader = csv.reader(csv_file, delimiter=',')
+        for row in reader:
+            if row[0] != "name" and row[0] != "code":
+                info = [row[0], int(row[1]), int(row[2])] # add data from csv file to list
+                addData = f"INSERT INTO {tablename} VALUES(?,?,?)" # accept parameters in values
+                c.execute(addData,info) # add parameters to addData command
 
 #roster table
 # c.execute('.mode column ') # DO THIS LATER FOR CONVENIENCE
@@ -21,29 +29,14 @@ c.execute((clearTable))
 command1 = "CREATE TABLE IF NOT EXISTS roster (name TEXT, age INTEGER, id INTEGER) " # create table if it does not exist with parameters
 c.execute((command1))    # run SQL statement
 
-with open('students.csv') as csv_file: # read students csv file
-    reader = csv.reader(csv_file, delimiter=',')
-    for row in reader:
-        if row[0] != "name":
-            info = [row[0], int(row[1]), int(row[2])] # add data from csv file to list
-            addData = "INSERT INTO roster VALUES(?,?,?)" # accept parameters in values
-            c.execute(addData,info) # add parameters to addData command
-
 #classes table
 clearTable = "DROP TABLE IF EXISTS classes" # clear if the tables exists
 c.execute((clearTable))
 command2 = "CREATE TABLE IF NOT EXISTS classes (name TEXT, mark INTEGER, id INTEGER) " # create table if it does not exist with parameters
 c.execute((command2))    # run SQL statement
 
-with open('courses.csv') as csv_file: # read classes csv file
-    reader = csv.reader(csv_file, delimiter=',')
-    for row in reader:
-        if row[0] != "code":
-            info = [row[0], int(row[1]), int(row[2])] # add data from csv file to list
-            addData = "INSERT INTO classes VALUES(?,?,?)" # accept parameters in values
-            c.execute(addData,info) # add parameters to addData command
-
-
+populate("students.csv", "roster")
+populate("courses.csv", "classes")
 #==========================================================
 
 db.commit() #save changes
