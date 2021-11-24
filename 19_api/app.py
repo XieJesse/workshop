254d7f1920a6abaa84e6e399bd67eda3618f2819
JesseStuy20 <jxie20@stuy.edu>
2021-11-23 19:20:04 -0500

    Got urllib working with json reading

--- 19_api/app.py
+++ 19_api/app.py
@@ -1,15 +1,21 @@
-from flask import Flask
+# Team Vengeful Mice: Raymond Yeung and Jesse Xie
+# SoftDev pd2
+# K19 -- A RESTful Journey Skyward
+# 2021-11-23
+
+from flask import Flask, render_template
 import json
-import urllib3
+import urllib
+
 app = Flask(__name__)
 
 @app.route("/")
 def test():
-    data = urllib2.urlopen('https://api.nasa.gov/planetary/apod?api_key=W4wJIqBrRcjVnc4PPIP6FJ8l64Jc1ecLi4Ye7dU4')
-    print(data.read())
-    response_info = json.loads(data)
-    print(response_info)
-    print("response_info")
-    return "No hablo queso!"
+    data = urllib.request.urlopen('https://api.nasa.gov/planetary/apod?api_key=W4wJIqBrRcjVnc4PPIP6FJ8l64Jc1ecLi4Ye7dU4')
+    response = data.read()
+    response_info = json.loads(response)
+    print(response_info['url'])
+    return render_template(main.html)
 
-app.run()
+if __name__ == "__main__":
+    app.run()
