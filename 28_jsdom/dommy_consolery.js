163fe33147760e286cbc4fb8b56422541dcac326
JesseStuy20 <jxie20@stuy.edu>
2022-02-08 22:19:00 -0500

    Removed block comment at top of js file

--- 28_jsdom/dommy_consolery.js
+++ 28_jsdom/dommy_consolery.js
@@ -1,22 +1,3 @@
-/*
-   your PPTASK:
-
-   Test drive each bit of code in this file,
-    and insert comments galore, indicating anything
-     you discover,
-    	have questions about,
-    		or otherwise deem notable.
-
-    		Write with your future self or teammates in mind.
-
-    		If you find yourself falling out of flow mode, consult
-    		other teams
-    		MDN
-
-   A few comments have been pre-filled for you...
-
-   (delete this block comment once you are done)
-*/
 // Team Tofu: Jesse Xie, Annabel Zhang, Justin Zou
 // SoftDev pd2
 // K28 -- Getting more comfortable with the dev console and the DOM
