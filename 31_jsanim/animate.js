57a5b250bd33a28e4604fd8a836ab0d1579a0a4a
JesseStuy20 <jxie20@stuy.edu>
2022-02-15 09:46:06 -0500

    Added classwork

---
+++ 31_jsanim/animate.js
@@ -0,0 +1,39 @@
+var c = document.getElementById('playground') ;
+var dotButton = document.getElementById('buttonCircle') ;
+var stopButton = document.getElementById('buttonStop') ;
+
+var ctx = c.getContext("2d") ;
+ctx.fillStyle = 'green' ;
+
+var requestID ;
+
+var clear = (e) => {
+  console.log("clear invoked...")
+  ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
+};
+
+var radius = 1 ;
+var growing = true ;
+
+var drawDot = () => {
+  console.log("drawDot invoked...")
+  clear() ;
+  ctx.beginPath() ;
+  ctx.arc(250,250,radius,0, 2 * Math.PI) ;
+  ctx.fill() ;
+  if (growing) {
+    radius++ ;
+  }
+  else {
+    radius -- ;
+  }
+  if (radius === 250) {
+    growing = false ;
+  }
+  if (radius === 0) {
+    growing = true ;
+  }
+
+}
+
+dotButton.addEventListener('click',drawDot)
